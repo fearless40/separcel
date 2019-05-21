808cbb3dca6621027a58bc90a69eced4fd90f280
Adam Spivack <spivack@outlook.com>
2019-05-21 16:43:03 -0700

    Updated my DB connection ideas. Working on a system that is flexable and easy to implement

--- src/database/dbapi.ts
+++ src/database/dbapi.ts
@@ -1,9 +1,85 @@
 import { User } from "../data/models/User";
 import { UserGroup } from "../data/models/UserGroups";
 
+
+const enum DBConnectionType {
+    Local = "local",
+    Server = "server"
+}
+
+interface DBResult {
+    from: DBConnectionType
+    errors?: [string]
+} 
+
+interface DBSendResult extends DBResult {
+    ok: boolean
+}
+
+interface DBGetResult extends DBResult {
+    values: [any]
+}
+
+const enum DBCompare {
+    Eq = 0,
+    GEq = 1,
+    GT = 2,
+    LEq = 3,
+    LT = 4,
+    NE = 5
+}
+
+interface DBQueryField {
+    field: string
+    value: string
+    comparison: DBCompare
+}
+
+interface DBQueryAll {
+    all: boolean
+}
+
+interface DBQueryBetween {
+    field: string
+    min: string
+    max: string
+}
+
+type DBQuery = DBQueryField | DBQueryAll | DBQueryBetween
+
+interface DBMsgGet {
+    table: string
+    query: {
+        by: DBQuery[]
+        fields?: string[]
+    }
+}
+
+interface DBMsgSend {
+    table: string
+    values: any[]
+}
+
+interface DataBaseConnection {
+    async send(msg: DBMsgSend): Promise<DBSendResult> 
+    async get(msg: DBMsgGet): Promise<DBGetResult>
+}
+
+
 class UsersFactory {
-    async getUsers(): Promise <User[]> {
-        return [];
+
+    constructor(private db: DataBaseConnection) {}
+
+    async getUsers(): Promise<User[]> {
+        const result = await this.db.get(
+            {
+                table: "user",
+                query: {
+                    by: [{all:true}],
+                    fields: ["lastname"]
+                }
+            }
+        });
     }
 
     async createUser(): Promise<User[]> {
